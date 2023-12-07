84bee966d11132d3fb0c97808b675e1d9337166c
Francisco Oliveira <francisco.viera@engdb.com.br>
2023-12-07 11:40:08 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/delley/imersao13/desafio01
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Pessoa struct {
+	Nome     string
+	Idade    int
+	Potuacao int
+}
+
+type porNome []Pessoa
+
+func (n porNome) Len() int           { return len(n) }
+func (n porNome) Less(i, j int) bool { return strings.ToLower(n[i].Nome) < strings.ToLower(n[j].Nome) }
+func (n porNome) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+type porIdade []Pessoa
+
+func (a porIdade) Len() int           { return len(a) }
+func (a porIdade) Less(i, j int) bool { return a[i].Idade < a[j].Idade }
+func (a porIdade) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func main() {
+
+	args := os.Args[1:]
+	fmt.Println(args)
+	if len(args) < 2 {
+		panic("informe o arquivo de entrada e de saída")
+	}
+	caminhoOrigem := args[0]
+	caminhoDestino := args[1]
+
+	arquivoOrigem, err := os.Open(caminhoOrigem)
+	if err != nil {
+		panic(err)
+	}
+	defer arquivoOrigem.Close()
+
+	reader := csv.NewReader(arquivoOrigem)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	pessoas := make([]Pessoa, len(records)-1)
+	for m, record := range records {
+		if m == 0 {
+			continue
+		}
+
+		idade, err := strconv.Atoi(strings.TrimSpace(record[1]))
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		pontuacao, err := strconv.Atoi(strings.TrimSpace(record[2]))
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		p := Pessoa{
+			Nome:     record[0],
+			Idade:    idade,
+			Potuacao: pontuacao,
+		}
+		pessoas[m-1] = p
+	}
+
+	sort.Sort(porNome(pessoas))
+
+	arquivoDestino, err := os.Create("por-nome-" + caminhoDestino)
+	if err != nil {
+		panic(err)
+	}
+	defer arquivoDestino.Close()
+
+	writer := csv.NewWriter(arquivoDestino)
+	//writer.Comma = ';'
+	defer writer.Flush()
+
+	headers := []string{"Nome", "Idade", "Pontuação"}
+
+	writer.Write(headers)
+	for _, c := range pessoas {
+		writer.Write([]string{c.Nome, strconv.Itoa(c.Idade), strconv.Itoa(c.Potuacao)})
+	}
+
+	sort.Sort(porIdade(pessoas))
+	arquivoDestino, err = os.Create("por-idade-" + caminhoDestino)
+	if err != nil {
+		panic(err)
+	}
+	defer arquivoDestino.Close()
+
+	writer = csv.NewWriter(arquivoDestino)
+	//writer.Comma = ';'
+	defer writer.Flush()
+
+	writer.Write(headers)
+	for _, c := range pessoas {
+		writer.Write([]string{c.Nome, strconv.Itoa(c.Idade), strconv.Itoa(c.Potuacao)})
+	}
+}
